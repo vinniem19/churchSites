d75ad53a730caa1e762b85c609482a27c849bf44
Vinnie <cuzvin2@gmail.com>
2019-09-18 17:51:43 -0400

    added map and some content

--- stmatt/script.js
+++ stmatt/script.js
@@ -1,8 +1,20 @@
-function myFunction() {
-    var x = document.getElementById("myLinks");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
-  }
+//$(document).ready(function (){
+ // $(".dropdown-trigger").dropdown({hover: false});
+// });
+
+$("button").click(function() {
+  // console.log("I am your God!");
+  let para = $("p");
+  para.text("I am the Lord your God!");
+  
+});
+
+
+// function myFunction() {
+//     var x = document.getElementById("myLinks");
+//     if (x.style.display === "block") {
+//       x.style.display = "none";
+//     } else {
+//       x.style.display = "block";
+//     }
+//   }
